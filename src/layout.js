f8b7701bdff6d9174830fcd8328f1941e0dda3e6
Natã Miccael Barbosa <natambarbosa@gmail.com>
2016-04-06 17:35:07 -0300

    scale fixed layout to fit container

--- src/layout.js
+++ src/layout.js
@@ -148,6 +148,11 @@ EPUBJS.Layout.Fixed.prototype.format = function(documentElement, _width, _height
 	//-- Adjust width and height
 	documentElement.style.width =  width + "px" || "auto";
 	documentElement.style.height =  height + "px" || "auto";
+	
+	//-- Scale to fit container
+	var scale = (_width + _height) / (parseInt(width) + parseInt(height));
+	documentElement.style.transform = 'scale(' + scale + ')';
+	documentElement.style.transformOrigin = '0 0';
 
 	//-- Remove columns
 	documentElement.style[columnWidth] = "auto";
